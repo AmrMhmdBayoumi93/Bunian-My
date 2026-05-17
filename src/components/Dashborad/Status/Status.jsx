d394178480694a79f2872169e8afbd0d1d17f383
Amr Muhammad <amb600680@gmail.com>
2026-05-17 16:10:46 +0300

    Finished the Card

--- src/components/Dashborad/Status/Status.jsx
+++ src/components/Dashborad/Status/Status.jsx
@@ -1,12 +1,40 @@
+
+  import React from "react";
+
+    import styles from "./Status.module.css";
+    import CardStatus from "../ui/CardStatus/CardStatus.jsx"
+
 function Status() {
   return (
-    <section>
+    <section className="py-4">
       <div className="container">
         <div className="row">
-          <div className="col-12 col-md-3">test 1</div>
-          <div className="col-12 col-md-3">test 2</div>
-          <div className="col-12 col-md-3">test 3</div>
-          <div className="col-12 col-md-3">test 4</div>
+
+         
+        
+      {/* // Alhamd LLAH  Done */} 
+
+          <div className="col-12 col-md-3"> 
+ 
+            <CardStatus counter= "120" title="Projects" icon ="fa-brands fa-r-project"   />
+
+          </div>
+
+          <div className="col-12 col-md-3"> 
+ 
+<CardStatus counter="50" title="Users" icon ="fa-solid fa-user" />
+          </div>
+
+          <div className="col-12 col-md-3"> 
+ 
+<CardStatus counter="30" title="Developers" icon ="fa-brands fa-dev" />
+          </div>
+
+          <div className="col-12 col-md-3"> 
+<CardStatus counter="10" title="Blogs" icon ="fa-solid fa-blog" />
+
+          </div>
+
         </div>
       </div>
     </section>
